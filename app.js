eca89d93d0f3e750a8ca1f404a98f21c1ba96711
Charles Rocha <rocha_charles@outlook.com>
2023-07-08 02:42:52 -0300

    Testando

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação está se conectando ao banco de dados");
+
+
+console.log("O Charles fez esse Commit para teste!");
